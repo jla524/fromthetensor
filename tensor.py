a9c84503d8a7f37cccc9ba8640604b63701c12a7
Jacky Lee <jla524@sfu.ca>
2022-04-11 20:55:45 -0700

    Simply tensor's print function

--- tensor.py
+++ tensor.py
@@ -5,14 +5,21 @@ import torch
 from numpy import ndarray
 
 
-def print_tensor(description: str, data: ndarray) -> None:
-    print(f'{description} = (\n{data}\n)\n')
+def print_tensor(description: str, tensor: torch.tensor) -> None:
+    """
+    Print the description and the data in the given tensor
+    :param description: a description of the tensor
+    :param tensor: a tensor initialized with PyTorch
+    :return: None
+    """
+    print(f'{description} = (\n{tensor.numpy()}\n)\n')
+
 
 a = torch.ones(3, 3)
 b = torch.eye(3, 3)
 
-print_tensor('a', a.numpy())
-print_tensor('b', b.numpy())
-print_tensor('a + b', (a + b).numpy())
-print_tensor('a * b', (a * b).numpy())  # matrix-matrix product
-print_tensor('matmul(a, b)', torch.matmul(a, b).numpy())
+print_tensor('a', a)
+print_tensor('b', b)
+print_tensor('a + b', a + b)
+print_tensor('a @ b', a @ b)  # matrix multiplication
+print_tensor('a * b', a * b)  # matrix-matrix product
