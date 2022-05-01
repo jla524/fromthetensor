d3503c854ecfbae62b5a796d92372caa71583f4e
Jacky Lee <jla524@sfu.ca>
2022-05-01 09:34:31 -0700

    Add function docstrings

--- pong.py
+++ pong.py
@@ -5,7 +5,7 @@ import os
 
 import gym
 import numpy as np
-from torch import nn
+from torch import nn, Tensor
 
 # Hyperparameters
 RENDER = os.getenv('RENDER') is not None
@@ -17,31 +17,39 @@ DIMENSIONS = 80 * 80
 
 
 class PongNet(nn.Module):
+    """
+    A neural net to play pong
+    """
     def __init__(self):
         super().__init__()
-        self.layer1 = nn.Linear(NUM_HIDDEN, DIMENSIONS)
-        self.layer2 = nn.Linear(DIMENSIONS)
+        self.layer1 = nn.Linear(DIMENSIONS, NUM_HIDDEN)
+        self.layer2 = nn.Linear(NUM_HIDDEN, 1)
         self.act1 = nn.ReLU()
         self.act2 = nn.Sigmoid()
 
     def forward(self, data):
+        """
+        Forward pass
+        """
         data = self.layer1(data)
         data = self.act1(data)
         data = self.layer2(data)
         data = self.act2(data)
         return data
 
+model = PongNet()
 
 # Game related
 if RENDER:
     env = gym.make('ALE/Pong-v5', render_mode='human')
 else:
     env = gym.make('ALE/Pong-v5')
-observation = env.reset()
-previous = None  # compute the difference in frames
 
 
 def preprocess(image):
+    """
+    Preprocess a 210x160x3 frame into a 6400 (80x80) float vector
+    """
     image = image[35:195]  # crop
     image = image[::2, ::2, 0]  # downsample by a factor of 2
     image[image == 144] = 0  # erase background type 1
@@ -49,10 +57,28 @@ def preprocess(image):
     image[image != 0] = 1  # everything else (paddles, ball) is set to 1
     return image.astype(np.float64).ravel()
 
-# Main loop
-while True:
-    current = preprocess(observation)
-    x = current - previous if previous is not None else np.zeros(DIMENSIONS)
-    previous = current
-    action = 0
-    observation, reward, done, info = env.step(action)
+
+def train():
+    """
+    Train the neural net to play pong
+    """
+    observation = env.reset()
+    previous = None
+
+    while True:
+        # Preprocess the observation
+        current = preprocess(observation)
+        frame = (current - previous if previous is not None
+                 else np.zeros(DIMENSIONS))
+        previous = current
+
+        # Forward the policy network
+        prob = model.forward(Tensor(frame))
+        action = 2 if np.random.uniform() < prob else 3
+
+        # Step the environment
+        observation, reward, done, info = env.step(action)
+
+
+if __name__ == '__main__':
+    train()
