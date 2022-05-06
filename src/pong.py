8a3d8b222b33b412654d41ca722dadd0bc856051
Jacky Lee <jla524@sfu.ca>
2022-05-05 20:28:10 -0700

    WIP use a loss function

--- src/pong.py
+++ src/pong.py
@@ -11,7 +11,7 @@ from torch import optim, nn, Tensor
 from torch.autograd import Variable
 
 # Hyperparameters
-LR = 1e-4
+LR = 1e-3
 GAMMA = 0.99
 DISCOUNT_FACTOR = 0.99
 RENDER = os.getenv('RENDER') is not None
@@ -19,8 +19,7 @@ RESUME = os.getenv('RESUME') is not None
 
 # Model initialization
 DIMENSIONS = 80 * 80
-NUM_HIDDEN1 = 200
-NUM_HIDDEN2 = 10
+NUM_HIDDEN = 200
 NUM_OUTPUT = 1
 BASE_PATH = Path(__file__).resolve().parent.parent
 MODEL_PATH = BASE_PATH / 'models'
@@ -33,9 +32,8 @@ class PongNet(nn.Module):
     """
     def __init__(self):
         super().__init__()
-        self.layer1 = nn.Linear(DIMENSIONS, NUM_HIDDEN1, bias=False)
-        self.layer2 = nn.Linear(NUM_HIDDEN1, NUM_HIDDEN2, bias=False)
-        self.layer3 = nn.Linear(NUM_HIDDEN2, NUM_OUTPUT, bias=False)
+        self.layer1 = nn.Linear(DIMENSIONS, NUM_HIDDEN, bias=False)
+        self.layer2 = nn.Linear(NUM_HIDDEN, NUM_OUTPUT, bias=False)
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
 
@@ -45,16 +43,19 @@ class PongNet(nn.Module):
         :param data: a frame in the pong game
         :return: a probability to move the paddle up
         """
-        data = self.relu(self.layer1(data))
-        data = self.relu(self.layer2(data))
-        data = self.sigmoid(self.layer3(data))
+        data = self.layer1(data)
+        data = self.relu(data)
+        data = self.layer2(data)
+        data = self.sigmoid(data)
         return data
 
 if RESUME and os.path.isfile(SAVE_PATH):
     model = torch.load(SAVE_PATH)
 else:
+    MODEL_PATH.mkdir(exist_ok=True)
     model = PongNet()
 optimizer = optim.Adam(model.parameters(), lr=LR)
+criterion = nn.SmoothL1Loss()
 
 # Game related
 if RENDER:
@@ -101,6 +102,7 @@ class PongAgent:
         self.observation = env.reset()
         self.previous = None
         self.rewards = []
+        self.probs = []
         self.losses = []
         self.reward_sum = 0
         self.running_reward = None
@@ -124,8 +126,8 @@ class PongAgent:
         """
         result = np.vstack(self.rewards)
         result = discount_rewards(result)
-        result -= np.mean(result)
-        result /= np.std(result)
+        #result -= np.mean(result)
+        #result /= np.std(result)
         return result
 
     def _update_parameters(self) -> None:
@@ -133,9 +135,10 @@ class PongAgent:
         Perform a backward pass and an optimzation step
         """
         episode_reward = self._get_episode_reward()
-        episode_loss = episode_reward * np.vstack(self.losses)
-        loss = Variable(Tensor(episode_loss), requires_grad=True).mean()
+        expected = episode_reward * np.vstack(self.losses)
+        predicted = torch.vstack(self.probs)
         optimizer.zero_grad()
+        loss = criterion(predicted, Tensor(expected))
         loss.backward()
         optimizer.step()
 
@@ -150,6 +153,7 @@ class PongAgent:
             running = self.running_reward * DISCOUNT_FACTOR
             current = self.reward_sum * (1 - DISCOUNT_FACTOR)
             self.running_reward = running + current
+
         print(f'episode reward total {self.reward_sum} '
               f'running reward {self.running_reward}')
 
@@ -161,6 +165,7 @@ class PongAgent:
         self.observation = env.reset()
         self.previous = None
         self.rewards = []
+        self.probs = []
         self.losses = []
         self.reward_sum = 0
 
@@ -169,16 +174,16 @@ class PongAgent:
         Train the neural net to play pong
         :return: None
         """
-        model.train()
         episode_number = 0
-        MODEL_PATH.mkdir(exist_ok=True)
+        model.train()
 
         while True:
             frame = self._get_frame()
             prob = model.forward(Tensor(frame))
             action = 2 if np.random.uniform() < prob else 3
             label = 1 if action == 2 else 0
-            self.losses.append(label - prob.item())
+            self.probs.append(prob)
+            self.losses.append(label)
 
             self.observation, reward, done, _ = env.step(action)
             self.reward_sum += reward
