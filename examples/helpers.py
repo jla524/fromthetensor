fa3d549bbe5d1dd157d64453319e75a00d7b2800
Jacky Lee <jla524@sfu.ca>
2023-03-09 09:20:22 -0800

    Support transform in train and evaluate

--- examples/helpers.py
+++ examples/helpers.py
@@ -5,11 +5,11 @@ import torch.nn.functional as F
 np.random.seed(1337)
 
 
-def train(model, X_train, Y_train, optim, steps, BS=128, lossfn=F.cross_entropy):
+def train(model, X_train, Y_train, optim, steps, BS=128, lossfn=F.cross_entropy, transform=lambda x: x):
     model.train()
     for i in (t := trange(steps)):
         sample = np.random.randint(0, high=X_train.shape[0], size=(BS))
-        x = torch.tensor(X_train[sample])
+        x = torch.tensor(transform(X_train[sample]))
         y = torch.tensor(Y_train[sample])
         out = model(x)
         loss = lossfn(out, y)
@@ -22,9 +22,9 @@ def train(model, X_train, Y_train, optim, steps, BS=128, lossfn=F.cross_entropy)
         t.set_description(f"loss {loss:.2f} accuracy {accuracy:.2f}")
 
 
-def evaluate(model, X_test, Y_test):
+def evaluate(model, X_test, Y_test, transform=lambda x: x):
     model.eval()
-    out = model(torch.tensor(X_test))
+    out = model(torch.tensor(transform(X_test)))
     preds = torch.argmax(out, dim=1).numpy()
     accuracy = (Y_test == preds).mean()
     print(f"test set accuracy is {accuracy}")
